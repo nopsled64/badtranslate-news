206c8a36ee2083a74c1263a042573798f702c735
nopsled <nopsled@spectrum.lan>
2019-06-09 18:17:15 +0100

    implemented RSS feed getter for the BBC News politics stream

--- main.go
+++ main.go
@@ -3,16 +3,26 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"os"
 )
 
 func main() {
-	badText, err := badTranslate("Michael Gove admits he was lucky to avoid jail over cocaine use")
+	// badText, err := badTranslate("Michael Gove admits he was lucky to avoid jail over cocaine use")
+	// if err != nil {
+	// 	fmt.Println("Error: ", err)
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Printf("Badly translated passage: %v\n", html.UnescapeString(badText))
+
+	rss, err := getNewsRSS()
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("Badly translated passage: %v\n", html.UnescapeString(badText))
+	for i := 0; i < len(rss.Channels[0].Items); i++ {
+		fmt.Println("News Title: " + rss.Channels[0].Items[i].Title)
+		fmt.Println("	Description: " + rss.Channels[0].Items[i].Description)
+	}
 }
---
+++ news_getter.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// RSS is this
+type RSS struct {
+	XMLName  xml.Name  `xml:"rss"`
+	Channels []Channel `xml:"channel"`
+}
+
+//Channel is this
+type Channel struct {
+	XMLName xml.Name `xml:"channel"`
+	Items   []Item   `xml:"item"`
+}
+
+// Item : this
+type Item struct {
+	XMLName     xml.Name `xml:"item"`
+	Title       string   `xml:"title"`
+	Description string   `xml:"description"`
+	pubDate     string   `xml:"pubDate"`
+}
+
+//function inspired by https://tutorialedge.net/golang/parsing-xml-with-golang/
+func getNewsRSS() (RSS, error) {
+	var rss RSS
+
+	if data, err := getContent("http://feeds.bbci.co.uk/news/politics/rss.xml"); err != nil {
+		log.Printf("Failed to get XML: %v", err)
+		return rss, err
+	} else {
+		log.Println("Received XML:")
+		log.Println(string(data))
+		xml.Unmarshal(data, &rss)
+	}
+
+	return rss, nil
+}
+
+//function taken from https://stackoverflow.com/questions/42717716/reading-content-from-http-get-in-golang
+func getContent(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("GET error: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Status error: %v", resp.StatusCode)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Read body: %v", err)
+	}
+
+	return data, nil
+}
